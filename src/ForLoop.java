ea0311f7feaea9ac4467c458dbfa03dbdfd6b0b2
Mariya Korzhavina <m.s.korzhavina@gmail.com>
2022-02-15 13:57:15 +0700

    final

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -2,14 +2,16 @@ public class ForLoop {
 
     public static void main(String[] args) {
 
+        System.out.println("Числа из диапазона [0,100], кратные 4, в обратном порядке: ");
+
         for (int i = 100; i >= 1; i--) {
-            if (checkFour(i)) {
+            if (dividedByFour(i)) {
                 System.out.print(i + " ");
             }
         }
     }
 
-    public static boolean checkFour(int j) {
+    public static boolean dividedByFour(int j) {
         return j % 4 == 0;
     }
 }
